ceb2b9ca484fdad56587492b17c3ff7aa51e7bf5
nnnkit <iankit17@gmail.com>
2019-08-03 15:31:11 +0530

    add onPreBootstrap for creating files

--- gatsby-theme-elementary/gatsby-node.js
+++ gatsby-theme-elementary/gatsby-node.js
@@ -1,8 +1,25 @@
-let basePath;
+const path = require("path");
+const fs = require("fs");
+let basePath, contentPath, assetPath;
 
-exports.onPreBootstrap = ({ reporter }, themeOptions) => {
+exports.onPreBootstrap = ({ reporter, store }, themeOptions) => {
   basePath = themeOptions.basePath || "/";
-  console.log(basePath);
+  contentPath = themeOptions.contentPath || "content/post";
+  assetPath = themeOptions.assetPath || "content/assets";
+
+  const {
+    program: { directory }
+  } = store.getState();
+  const dirs = [
+    path.join(directory, contentPath),
+    path.join(directory, assetPath)
+  ];
+  dirs.forEach(dir => {
+    reporter.info(`Creating ${dir} directory`);
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+  });
 };
 
 exports.createPages = ({ actions, reporter }) => {
